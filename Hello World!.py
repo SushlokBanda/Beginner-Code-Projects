cf2b0e019143abd2bff5beb1ff9f8d693f7f6f9c
Sushlok Banda <147444540+SushlokBanda@users.noreply.github.com>
2024-02-07 20:26:28 -0500

    Add files via upload

---
+++ Hello World!.py
@@ -0,0 +1,9 @@
+# Hello World!
+print("Hello, World!")
+
+# About Me
+print("Hello, I'm Sushlok")
+print("I'm Interested in Cubing and Technology")
+print("I want to Major in Computer Science")
+print("My Dream Career is to be Software Developer")
+
